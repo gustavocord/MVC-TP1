75ba63a62b3b8214723c51f6986c3f3506d47af6
gustavo cordoba <cordobagustavoj@gmail.com>
2021-08-22 11:46:20 -0300

    botones OK-falta funcionalidad

--- TP1-P3/src/view/Tabla.java
+++ TP1-P3/src/view/Tabla.java
@@ -99,56 +99,9 @@ public class Tabla extends JFrame {
 		
 	}
 	
-	// hay que implementar bien esto
-	/*
-	private class ButtonPress implements ActionListener {
-		
 	
-		@Override
-		public void actionPerformed(ActionEvent action) {
-		
-			boolean done = false;
-			int x = 0;
-			int y = 0;
-			for (int i = 0; i < tablero.getBotones().length; i++) {
-				for (int j = 0; j < tablero.getBotones()[i].length; j++) {
-					if (((JButton) action.getSource()).equals(tablero.getBotones()[i][j])) {
-						done = true;
-						x = i;
-						y = j;
-						break;
-					}
-				}
-				if (done) {
-					break;
-				}
-			}
-			cambiarColor(tablero.getBotones()[x][y]);//cambia el color donde se hizo click
-			if (x > 0) {// Check if there is a space available to the left of the selected tile
-				cambiarColor(tablero.getBotones()[x - 1][y]);
-			}
-			if (x < tablero.getBotones().length - 1) {// Check if there is a space available to the right of the selected tile
-				cambiarColor(tablero.getBotones()[x + 1][y]);
-			}
-			if (y > 0) {// Check if there is a space available to the top of the selected tile
-				cambiarColor(tablero.getBotones()[x][y - 1]);
-			}
-			if (y < tablero.getBotones()[x].length - 1) {// Check if there is a space available to the bottom of the selected tile
-				cambiarColor(tablero.getBotones()[x][y + 1]);
-			}
-			
-		}
-		
-
-		public void cambiarColor(Boton boton) {//cambiamos el estado depende el color
-			
-			if (boton.estado()) {
-				
-				boton.cambiarEstado(false);;
-			} else {
-				boton.cambiarEstado(true);
-			}
-		}
-} */
+	/*
+	 * realizar funcionalidad de botones con matriz
+*/
 }
 
