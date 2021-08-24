69a10901cd618ee0585e2a972f0a686059733090
gustavo cordoba <cordobagustavoj@gmail.com>
2021-08-23 22:16:40 -0300

    funciona boton , falta ver vista

--- TP1-P3/src/view/Tabla.java
+++ TP1-P3/src/view/Tabla.java
@@ -22,12 +22,11 @@ import java.awt.Color;
 
 
 
-public class Tabla extends JFrame {
+public class Tabla extends JFrame  implements ActionListener {
 
 	JFrame frame;
 	private Tablero tablero;
-	
-	
+	JButton [][]botones; //va a estar ubicado igual que el que viene en la logica
 	
 	
 	/**
@@ -58,6 +57,7 @@ public class Tabla extends JFrame {
 	public Tabla() {
 		super(" juego de luces");
 		this.tablero = new Tablero(4,4);
+		this.botones=new JButton[4][4];
 	
 		initialize();
 	}
@@ -79,29 +79,72 @@ public class Tabla extends JFrame {
 			for(int j = 0; j < tablero.getBotones()[i].length; j++) {
 				// crea el boton y luego dependiendo el estado le pone el color de fondo
 				
+				
 				JButton botn = new JButton("");
-				if(tablero.getBotones()[i][j].estado()) {
+				botones[i][j]=botn;
+				if(tablero.getBotones()[i][j].getEstado()) {
 					botn.setBorderPainted(false);
 					botn.setBackground(Color.black);
+					botn.addActionListener(new ButtonPress());
 				}
 				else {
 					botn.setBorderPainted(false);
 					botn.setBackground(Color.green);
+					botn.addActionListener(new ButtonPress());
 				}
 				
 				frame.getContentPane().add(botn);
 			}
-		}
-		
-		
-		
-		
+		}	
 		
 	}
+
+	private class ButtonPress implements ActionListener {	
 	
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		
+		//if(event.getSource()==botn) {
+			//System.out.println("se presiono");
+			boolean presionado = false;
+			
+			int i = 0, j = 0;
+	        while (i < botones.length && !presionado) {
+	 
+	        	if (((JButton) event.getSource()).equals(botones[i][j])) {
+	        		presionado= true;
+	        		System.out.println("se presiono");
+	        	}
+	            
+	            if (j == botones[0].length - 1) {
+	                i++;
+	                j = 0;
+	            } else {
+	                j++;
+	            }
+	 
+	        }
+	        
+	        tablero.cambiarEstado(i, j);
+	        
+	        
+	        
+		}
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	}
 	
 	/*
 	 * realizar funcionalidad de botones con matriz
 */
-}
+	
+		
+
+
 
